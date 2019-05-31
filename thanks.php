8dcace8c6d7bdca8cec77ac403dc02f9633d742c
nexseedcamille <ykamimori@nexseed.net>
2019-05-31 15:56:11 +0800

    bd操作用のファイル追加
    DB操作ができるように設定

--- thanks.php
+++ thanks.php
@@ -13,9 +13,11 @@
     $email = h($_POST['email']);
     $content = h($_POST['content']);
 
+    //DBとの接続
     require_once('dbconnect.php');
     $stmt = $dbh->prepare('INSERT INTO surveys (nickname, email, content) VALUES (?, ?, ?)');
     $stmt->execute([$nickname, $email, $content]);//?を変数に置き換えてSQLを実行
+
 ?>
 <!DOCTYPE html>
 <html lang="ja">
